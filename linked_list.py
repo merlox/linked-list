86a486515af6a7fa9de4c5ce090b17c1774c92f5
merlox <merunasgrincalaitis@gmail.com>
2019-08-12 14:36:35 +0200

    First commit

---
+++ linked_list.py
@@ -0,0 +1,105 @@
+# -*- encoding: utf-8 -*-
+
+class Node:
+    """
+    A class for storing a singly linked list node
+    Models two attributes – data and the next node in the linked list
+    """
+    data = None
+    next = None
+    def __init__(self, data=None):
+        self.data = data
+
+    def __repr__(self):
+        return "{ Node data: %s -> next: %s }" % (self.data, self.next.data if self.next else None)
+
+class SinglyLinkedList:
+    """
+    Singly linked list
+    """
+    head = None
+    def __init__(self, head=None):
+        self.head = head
+
+    def __repr__(self):
+        nodes = []
+        current = self.head
+
+        while current:
+            if current is self.head:
+                nodes.append("[Head %s]" % current.data)
+            elif current.next is None:
+                nodes.append("[Tail %s]" % current.data)
+            else:
+                nodes.append("[%s]" % current.data)
+            current = current.next
+
+        return ' -> '.join(nodes)
+
+    def is_empty(self):
+        return self.head == None
+
+    def prepend(self, first_node):
+        """
+        Adds a new node to the head of the list
+        Takes O(1) time
+        """
+        first_node.next = self.head
+        self.head = first_node
+
+    def size(self):
+        """
+        Returns the length of the list
+        Takes O(n) time (Linear Time)
+        """
+        current = self.head
+        total_size = 0
+        while current != None:
+            total_size += 1
+            current = current.next
+        return total_size
+
+    def search(self, key):
+        """
+        Search for the first node containing the date of the key
+        Returns the node or None if not found
+        Takes O(n) time
+        """
+        current = self.head
+        while current:
+            if current.data == key:
+                return current
+            current = current.next
+        return None
+
+    def insert(self, new_node, position):
+        """
+        Inserts a node in the position of your choice or at the end if it exceeds the size of it
+        """
+        if position == 0:
+            self.prepend(new_node)
+
+        if position >= self.size():
+            position = self.size()
+
+        previous_node = self.head
+        next_node = previous_node.next
+        for i in range(position + 1):
+            if i == 0: continue
+
+            if i == position:
+                previous_node.next = new_node
+                new_node.next = next_node
+                break
+            previous_node = next_node
+            next_node = next_node.next
+
+N1 = Node(10)
+N2 = Node(20)
+N3 = Node(30)
+N4 = Node(40)
+N5 = Node(50)
+
+list = SinglyLinkedList(N1)
+list.prepend(N2)
+list.prepend(N3)
